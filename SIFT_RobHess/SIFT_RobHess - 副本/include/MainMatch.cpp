242b250e8daec0d33f609ee2eee84d78d9117b10
zcg1942 <1597225629@qq.com>
2018-04-24 20:00:19 +0800

    Revert "pca之后删除debug文件才回到master"
    This reverts commit 9c6f76cc85fa91a69ebed19c15010acad26a1efb.

--- SIFT_RobHess/SIFT_RobHess - 副本/include/MainMatch.cpp
+++
@@ -1,3 +0,0 @@
-#include<match.h>
-int argc, char** argv;
-argv[1]
--- SIFT_RobHess/SIFT_RobHess - 副本/include/getpsnr.h
+++
@@ -1,5 +0,0 @@
-#include <opencv2/core/core.hpp>        // Basic OpenCV structures (cv::Mat, Scalar)
-#include <opencv2/highgui/highgui.hpp>  // OpenCV window I/O
-//using namespace cv;
-//double getPSNR(const Mat, const Mat);
-void psnr(IplImage * , IplImage * );
